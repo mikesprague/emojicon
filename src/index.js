671abdd98ed9c6c92fab25efd213464b59b7e600
Michael Sprague <mikesprague@gmail.com>
2022-11-29 21:43:46 -0500

    refactor: ♻️  improve logic
    Signed-off-by: Michael Sprague <mikesprague@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ export default {
   }
 }
 
+const extractEmoji = (input) =>
+  input.match(/\p{Emoji_Presentation}|\p{Symbol}/gu);
+
+const cleanEmoji = (emoji) =>
+  emoji.replace(/&/g, '&amp;').replace(/</g, '&lt;');
+
+const emojiToSvg = (emoji) =>
+  `<svg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='0 0 17 17'><text x='-2' y='13'>${emoji}</text></svg>`;
+
 async function handleRequest(request) {
+  let returnEmoji = cleanEmoji('🚫');
   const url = new URL(request.url);
-  const emoji = decodeURIComponent(url.pathname).replace('/', '');
-  const cleanEmoji = emoji.replace(/&/g, '&amp;').replace(/</g, '&lt;');
-  const returnBody = `<svg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='0 0 17 17'><text x='-2' y='13'>${cleanEmoji}</text></svg>`;
+  const pathParam = decodeURIComponent(url.pathname).replace('/', '');
+  const emojiArray = extractEmoji(pathParam);
+
+  if (emojiArray && emojiArray.length) {
+    returnEmoji = cleanEmoji(emojiArray[0]);
+  }
+
+  const returnBody = emojiToSvg(returnEmoji);
 
   return new Response(returnBody, {
     headers: {
