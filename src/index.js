e64629cee141181b1575c5ddf435ba04ea7619b2
Michael Sprague <mikesprague@gmail.com>
2022-11-30 11:07:27 -0500

    style: 🎨 fix linebreaks/whitespace

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 export default {
   async fetch(request, env) {
-    return await handleRequest(request)
-  }
-}
+    return await handleRequest(request);
+  },
+};
 
 const extractEmoji = (input) =>
   input.match(/\p{Emoji_Presentation}|\p{Symbol}/gu);
@@ -11,7 +11,7 @@ const cleanEmoji = (emoji) =>
   emoji.replace(/&/g, '&amp;').replace(/</g, '&lt;');
 
 const emojiToSvg = (emoji) =>
-  `<svg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='0 0 17 17'><text x='-2' y='13'>${emoji}</text></svg>`;
+  `<svg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='0 0 17 17'><text x='0' y='13'>${emoji}</text></svg>`;
 
 async function handleRequest(request) {
   let returnEmoji = cleanEmoji('🚫');
